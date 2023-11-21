85ea590fd855c7fc8def4da50ed1ded49173094b
Mayla Bisacchi <Mayla.Bisacchi@sptech.school>
2023-11-21 14:48:12 -0300

    Criação de aceite de politica lgpd

---
+++ app/src/main/java/maitre/app/layout/AceiteLgpd.kt
@@ -0,0 +1,12 @@
+package maitre.app.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import maitre.app.R
+
+class AceiteLgpd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aceite_lgpd)
+    }
+}
