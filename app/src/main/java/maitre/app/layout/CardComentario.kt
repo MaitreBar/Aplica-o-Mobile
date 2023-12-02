ef572d063ff912dc21254db64f70cdc2f35025e2
BeatrizFrancaGusmaoo <beatriz.gusmao@sptech.school>
2023-12-01 22:37:05 -0300

    Dark mode nas telas criação reserva, feedback, ajustes em bugs em feedback, alteração no card comentario

--- app/src/main/java/maitre/app/layout/CardComentario.kt
+++ app/src/main/java/maitre/app/layout/CardComentario.kt
@@ -9,21 +9,14 @@ import maitre.app.R
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Card_Comentario.newInstance] factory method to
+ * Use the [CardComentario.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Card_Comentario : Fragment() {
+class CardComentario : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(maitre.app.ARG_PARAM1)
-            param2 = it.getString(maitre.app.ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,13 +36,6 @@ class Card_Comentario : Fragment() {
          * @return A new instance of fragment Card_Comentario.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Card_Comentario().apply {
-                arguments = Bundle().apply {
-                    putString(maitre.app.ARG_PARAM1, param1)
-                    putString(maitre.app.ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
